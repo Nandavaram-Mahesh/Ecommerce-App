c534331436da868c28264a612f123ef63f071df2
Mahesh <mahipara2021@gmail.com>
2022-03-04 10:51:57 +0530

    prime and image update

--- src/Home.js
+++ src/Home.js
@@ -19,10 +19,10 @@ const productDetailsobject=[
     },
     {
         id:uuidv4(),
-        imageUrl:'https://m.media-amazon.com/images/I/61Li3cp0wvL._AC_SL1500_.jpg',
-        title:'Smart Watch',
+        imageUrl:'https://res.cloudinary.com/maheshsf9/image/upload/v1646369430/smartphone_uh3smw.png',
+        title:'Smart Phone',
         rating:5,
-        price:59.99,
+        price:159.99,
     },
 ]
 const Home = () => {
@@ -41,16 +41,14 @@ const Home = () => {
 
             </div>
             <div className='home-row'>
-                <Product id={678} image="https://www.reliancedigital.in/medias/Amazon-Echo-dot-3-0-BK-Computer-Speaker-491431035-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w3NjM2NHxpbWFnZS9qcGVnfGltYWdlcy9oYjUvaGY3LzkwNTk0NDcwNDYxNzQuanBnfGFlZjBlOTZkMzY4MmI0ZTBhMDMzZWRiMjM1MjE2OWYxYTFmZWMxMWJlNjY2YzI5Nzc4ODM0ZWU2MWNlYjY0ZjE" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={91011} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={121314} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={141516} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={171819} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={202122} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
+                <Product id={678} image="https://www.reliancedigital.in/medias/Amazon-Echo-dot-3-0-BK-Computer-Speaker-491431035-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w3NjM2NHxpbWFnZS9qcGVnfGltYWdlcy9oYjUvaGY3LzkwNTk0NDcwNDYxNzQuanBnfGFlZjBlOTZkMzY4MmI0ZTBhMDMzZWRiMjM1MjE2OWYxYTFmZWMxMWJlNjY2YzI5Nzc4ODM0ZWU2MWNlYjY0ZjE" title="Alexa" rating={5} price={229.99}/>
+                <Product id={91011} image="https://res.cloudinary.com/maheshsf9/image/upload/v1646369459/Bed_iixhpp.png" title="Double Cart Bed" rating={5} price={3529.99}/>
+                <Product id={121314} image="https://res.cloudinary.com/maheshsf9/image/upload/v1646369449/washing_machine_qu9dnf.png" title="Smart Washing Machine" rating={5} price={1699.99}/>
+                <Product id={141516} image="https://res.cloudinary.com/maheshsf9/image/upload/v1646369442/fridge_qceieu.png" title="Smart Fridge" rating={5} price={1379.99}/>
+                
             </div>
             <div className='home-row'>
-                <Product id={232425} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>
-                <Product id={262728} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" title="The Lean Startup" rating={5} price={29.99}/>       
+            <Product id={171819} image="https://res.cloudinary.com/maheshsf9/image/upload/v1646369437/Mixer_Grinder_i4ixgx.png" title="The Mixer Grinder" rating={5} price={329.99}/>
             </div>
             </div>
         </div>
